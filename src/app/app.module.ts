de08b4e0f3d554dc25fc6730d5111752bd3effd9
batibot323 <hani.herbert.ho@gmail.com>
2019-12-17 19:09:48 +0800

    Copied code from https://www.telerik.com/kendo-angular-ui/components/dialogs/.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DialogsComponent } from './dialogs/dialogs.component';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DialogsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
